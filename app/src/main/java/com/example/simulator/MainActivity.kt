2e1892f49ca46c0a0cc394e3c0c65656da042796
Felipe Tomasello <felipe.tomasello@gmail.com>
2022-08-01 19:23:56 -0300

    First Commit

---
+++ app/src/main/java/com/example/simulator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.simulator
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.simulator.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
